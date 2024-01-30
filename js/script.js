a0f630ef4cc25bf1d5bea4b79860bea897e67244
Catalogo81 <peacematjato@gmail.com>
2024-01-30 23:33:33 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =============================================== typing animation ================================================== */
+var typed = new Typed(".typing", {
+    strings:["", "Software Developer", "IT Technician", "System Administrator"],
+    typedSpeed:10,
+    BackSpeed:10,
+    loop:true
+})
